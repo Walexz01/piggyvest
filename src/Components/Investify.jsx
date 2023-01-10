1341e09de3bc527c99e79b3ac19844445350b1f2
walexz <adegbiteadewaleoluwaseun@gmail.com>
2023-01-10 23:16:47 +0100

    complete the right side of the investify

--- src/Components/Investify.jsx
+++ src/Components/Investify.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import InvestifyImg from '../piggy-image/home_page_images/invest.png'
 import Investifydot from '../piggy-image/home_page_images/investify__dot.png'
 const Investify = () => {
@@ -10,7 +11,17 @@ const Investify = () => {
                 <img src={Investifydot} alt=""  className='investify__dot'/>
             </div>
             <div className="investify__right">
-                this is the right side Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ipsa provident, sapiente sit beatae eum suscipit vero quas praesentium earum perspiciatis repellat possimus recusandae. Iusto nulla quod porro temporibus distinctio perferendis placeat dignissimos fuga ipsa quasi magnam cumque, pariatur provident perspiciatis magni aspernatur ipsam enim sunt ratione error. Vitae, dignissimos esse. Ad animi illo rem aut numquam magnam dolorem natus iure sed architecto quam quisquam, ea alias placeat dolor officia ipsa est deserunt provident id. Iusto perferendis molestias corporis quidem dolores odio nisi et in officia. Et magnam, provident excepturi iure doloremque nesciunt beatae rem veritatis, ullam, atque id repudiandae.
+                <div className="investify__content">                    
+                    <small className="investify__tag">
+                    Up To 25% Returns
+                    </small>
+                    <h3>
+                    Access investment opportunities
+                    </h3>
+                    <p>Invest securely and confidently on the go. Grow your money confidently by investing in pre-vetted investment opportunities.</p>
+                    <Link>Learn more about Investments &gt; </Link>
+                </div>
+
             </div>
         </div>
     </section>
