93dc26b3d8a97913573495154df6fa991fa318ca
walexz <adegbiteadewaleoluwaseun@gmail.com>
2023-01-28 23:44:48 +0100

    worked on reponsiveness for 935px and 768px

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -3,51 +3,59 @@ import { Link, NavLink, Outlet } from 'react-router-dom'
 import  Logo from "../piggy-image/home_page_images/logo 2.svg";
 import { NavLinks } from '../data';
 import { useState } from 'react';
+import {GoThreeBars} from 'react-icons/go'
+import {TiTimes} from 'react-icons/ti'
+
 const Layout = () => {
     const [modalVisible, setmodalVisible] = useState(false)
+    const [navVisible, setNavVisible] = useState(false)
   return (
     <>
         <header>
         <nav className='nav_bar'>
-            <div className="nav_bar--right">
                 <div className="nav_bar--logo" >
-                    <Link to='/'>
+                    <Link to='/' onClick={()=> setNavVisible(false)}>
                     <img src={Logo} alt=""  />
                     </Link>
                 </div>
-                <ul className='nav_bar--list'>
-                    { NavLinks.map(({id,name,path,modal})=>{
-                        return(
-                            <li  onMouseEnter={modal ?  ()=> setmodalVisible(true): null} onMouseLeave={modal ?  ()=> setmodalVisible(false): null} key={id}>
-                                <NavLink  to={path}>{name}</NavLink>
-                                { modal && modalVisible &&
-                                    <div className="modal">
-                                        { modal.map(({name,link,image},index)=>{
-                                            return(
-                                    <Link key={index} className="link__img" to={link}>
-                                        <img src={image} alt='link icon' />
-                                        {name}
-                                    </Link>
-                                        ) 
+                <div className={ navVisible ? 'links open': 'links'}>                    
+                    <ul className='nav_bar--list'>
+                        { NavLinks.map(({id,name,path,modal})=>{
+                            return(
+                                <li onClick={()=> setNavVisible(false)} onMouseEnter={modal ?  ()=> setmodalVisible(true): null} onMouseLeave={modal ?  ()=> setmodalVisible(false): null} key={id}>
+                                    <NavLink  to={path}>{name}</NavLink>
+                                    { modal && modalVisible &&
+                                        <div className="modal">
+                                            { modal.map(({name,link,image},index)=>{
+                                                return(
+                                        <Link key={index} className="link__img" to={link}>
+                                            <img src={image} alt='link icon' />
+                                            {name}
+                                        </Link>
+                                            ) 
 
-                                        })
-                                        }
-                                </div>
-                                }
-                            </li>
-                            )
-                    })
+                                            })
+                                            }
+                                    </div>
+                                    }
+                                </li>
+                                )
+                        })
+                        }
+                    </ul>
+                    <div className="ctn__container">
+                        <div className="sign_in btn">
+                            <Link to='login'>Sign in</Link>
+                        </div>
+                        <div className="create_acct btn">
+                            <Link to='signup'>Create free account</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="close" onClick={()=> setNavVisible(prev => !prev)}>
+                    {
+                        navVisible ? <TiTimes/> : <GoThreeBars/>
                     }
-                </ul>
-            </div>
-
-                <div className="nav_bar--left">
-                    <div className="sign_in btn">
-                        <Link to='login'>Sign in</Link>
-                    </div>
-                    <div className="create_acct btn">
-                        <Link to='signup'>Create free account</Link>
-                    </div>
                 </div>
             </nav>
         </header>    
